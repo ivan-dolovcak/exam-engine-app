176097497757258e329df697a0263e53a9029854
Ivan Dolovčak <idolovcak7@gmail.com>
2024-04-01 23:54:26 +0200

    feat: create basic website skeleton
    - PHP config, Apache config, include files

---
+++ htdocs/include/config.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+if (isset($_SERVER["DEVELOPMENT"])) {
+    error_reporting(E_ALL);
+    ini_set("display_errors", true);
+    ini_set("display_startup_errors", true);
+    
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+}
+
+date_default_timezone_set("UTC");
+
+# Auto-require classes:
+spl_autoload_register(fn($className) => require "$className.php");
