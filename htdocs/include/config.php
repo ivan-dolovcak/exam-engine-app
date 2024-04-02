89442e418370f47aa387b163fa3f8736fd6ec915
Ivan Dolovčak <idolovcak7@gmail.com>
2024-04-02 04:04:15 +0200

    style: added newlines before EOF's, fix HTML

--- htdocs/include/config.php
+++ htdocs/include/config.php
@@ -5,7 +5,7 @@ if (isset($_SERVER["DEVELOPMENT"])) {
     error_reporting(E_ALL);
     ini_set("display_errors", true);
     ini_set("display_startup_errors", true);
-    
+
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 }
 
