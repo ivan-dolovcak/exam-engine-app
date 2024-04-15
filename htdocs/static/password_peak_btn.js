afad4c7aad0b7fcb34722c975ca08e52fe77bb03
Ivan Dolovčak <idolovcak7@gmail.com>
2024-04-15 21:45:18 +0200

    feat: add password peak buttons

---
+++ htdocs/static/password_peak_btn.js
@@ -0,0 +1,6 @@
+const passwordInput = document.getElementById("password");
+
+document.getElementById("btn-password-peak").addEventListener(
+    "mousedown", () => passwordInput.type = "text");
+document.getElementById("btn-password-peak").addEventListener(
+    "mouseup", () => passwordInput.type = "password");
