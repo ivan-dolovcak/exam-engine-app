11859179e47b96e9aea0eed31c3ed7856b3ff11a
Ivan Dolovčak <idolovcak7@gmail.com>
2024-04-21 22:51:37 +0200

    feat: create document table

--- sql/02_schema.sql
+++ sql/02_schema.sql
@@ -13,3 +13,21 @@ create table `User` (
     constraint `UK_username`    unique key (`username`),
     constraint `UK_email`       unique key (`email`)
 );
+
+create table `Document` (
+    `ID`                  mediumint unsigned not null auto_increment,
+    `name`                varchar(50) not null,
+    `type`                enum("exam", "form") not null,
+    `visibility`          enum("public", "unlisted", "private")
+                          not null default "private",
+    `numMaxSubmissions`   tinyint unsigned default 1,
+    `authorID`            mediumint unsigned not null,
+    `deadlineDatetime`    datetime,
+    `creationDate`        date not null default utc_date(),
+    `documentJSON`        json,
+    `solutionJSON`        json,
+    primary key (`ID`),
+    constraint `FK_author`
+        foreign key (`authorID`) references `User`(`ID`)
+        on delete cascade
+);
