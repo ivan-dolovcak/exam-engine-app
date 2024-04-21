b2cef873e24c406c95082b11afe59b9837045705
Ivan Dolovčak <idolovcak7@gmail.com>
2024-04-21 12:46:01 +0200

    fix: CSS transitions, pwd btn peak accessibility

--- htdocs/static/js/password_peak_btn.js
+++ htdocs/static/js/password_peak_btn.js
@@ -4,3 +4,6 @@ document.getElementById("btn-password-peak").addEventListener(
     "mousedown", () => passwordInput.type = "text");
 document.getElementById("btn-password-peak").addEventListener(
     "mouseup", () => passwordInput.type = "password");
+document.getElementById("btn-password-peak").addEventListener(
+    "click", () => passwordInput.type
+        = passwordInput.type === "password" ? "text" : "password");
