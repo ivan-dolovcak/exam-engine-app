4227731aa86c54709e37d75bc0ded362601d32de
Ivan Dolovčak <idolovcak7@gmail.com>
2024-04-27 10:48:42 +0200

    optimize: decrease global scope littering

--- htdocs/static/js/init.js
+++ htdocs/static/js/init.js
@@ -8,15 +8,18 @@ function getCookie(cookieName)
     return null;
 }
 
-// Set theme:
 const preferences = JSON.parse(getCookie("examEnginePreferences"));
-const themeClassName = `${preferences.theme}-theme`;
-document.documentElement.classList.add(themeClassName);
 
-document.documentElement.lang = preferences.lang;
+window.addEventListener("DOMContentLoaded", () => {
+    // Set theme:
+    const themeClassName = `${preferences.theme}-theme`;
+    document.documentElement.classList.add(themeClassName);
 
-// Highlight current page anchor in nav:
-const currentPage = location.pathname;
-const a = document.querySelector(`nav a[href="${currentPage}"]`);
-if (a)
-    a.classList.add("active");
+    document.documentElement.lang = preferences.lang;
+
+    // Highlight current page anchor in nav:
+    const currentPage = location.pathname;
+    const a = document.querySelector(`nav a[href="${currentPage}"]`);
+    if (a)
+        a.classList.add("active");
+});
--- htdocs/static/js/password_peak_btn.js
+++ htdocs/static/js/password_peak_btn.js
@@ -1,6 +1,8 @@
-const passwordInput = document.getElementById("password");
+window.addEventListener("DOMContentLoaded", () => {
+    const passwordInput = document.getElementById("password");
 
-document.getElementById("btn-password-peak").addEventListener(
-    "mousedown", () => passwordInput.type = "text");
-document.getElementById("btn-password-peak").addEventListener(
-    "mouseup", () => passwordInput.type = "password");
+    document.getElementById("btn-password-peak").addEventListener(
+        "mousedown", () => passwordInput.type = "text");
+    document.getElementById("btn-password-peak").addEventListener(
+        "mouseup", () => passwordInput.type = "password");
+});
